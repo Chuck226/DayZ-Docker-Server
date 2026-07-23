d229f16bbc92cb9fe163b58bb60ddf53dd0b1b12
Chuck226 <suhaj.marcelko@gmail.com>
2026-07-23 03:59:06 +0200

    v0.5:   - resolved some bugs   - started on support for workshop mods   - plan to migrate this container to same steamcmd setup as on BI wiki   - currently workshop is not working

--- scripts/install_mods.sh
+++ scripts/install_mods.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 #this script will install mods from workshop and do their basic setup
 set -e
+
+download_mods() {
+    installed_mods=$(cat modlist.txt)
+
+    if [ "${WORKSHOP_MODS}" == "${installed_mods}" ]; then
+        steamcmd +force_install_dir /home/dayz-docker/server/dayz-server/ +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +app_update 223350 +quit
+        return 0
+    else
+        echo "${WORKSHOP_MODS}" > modlist.txt
+        var_mods="${WORKSHOP_MODS}"
+        var_mods="${var_mods//;/ }"
+        for i in ${var_mods}; do
+            steamcmd +force_install_dir /home/dayz-docker/server/dayz-server/ +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +workshop_download_item 221100 "${i}" +quit
+        done
+
+        for i in ${var_mods}; do
+            ln -s /home/dayz-docker/server/dayz-server/steamapps/workshop/content/221100/"${i}" /home/dayz-docker/server/dayz-server/"${i}"
+            ln -s /home/dayz-docker/server/dayz-server/steamapps/workshop/content/221100/"${i}"/keys/* /home/dayz-docker/server/dayz-server/keys/
+        done
+
+        return 0
+    fi
+}
+
+mkdir -p /home/dayz-docker/server/modlist
+cd /home/dayz-docker/server/modlist
+
+if [ ! -d "/home/dayz-docker/server/modlist/modlist.txt" ]; then
+    touch modlist.txt
+fi
+
+if [ -z "${WORKSHOP_MODS}" ]; then
+    exit 0
+else
+    download_mods
+    exit 0
+fi
--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -4,35 +4,34 @@ set -e
 
 install_server_as_requested_user() {
     mkdir -p /home/dayz-docker/server/dayz-server
-    steamcmd  +force_install_dir /home/dayz-docker/server/dayz-server/ +login "$STEAM_USERNAME" "$STEAM_PASSWORD" @NoPromptForPassword 1 +app_update 223350 +quit
+    steamcmd +@NoPromptForPassword 1 +force_install_dir /home/dayz-docker/server/dayz-server/ +login ${STEAM_USERNAME} ${STEAM_PASSWORD} ${STEAM_GUARD_CODE} +app_update 223350 +quit
 }
 
 #In this step we also need to bring back data from backup (validation/update can corrupt them)
 check_if_updated() {
     if [ ! -d "/home/dayz-docker/server/dayz-server/mpmissions" ] && [ -d "/home/dayz-docker/server/savefile-backup-folder/mpmissions" ]; then
-        steamcmd +force_install_dir /home/dayz-docker/server/dayz-server/ +login "$STEAM_USERNAME" +app_update 223350 +quit
+        bash /scripts/install_mods.sh
         rm -rf /home/dayz-docker/server/dayz-server/mpmissions
         cp -r /home/dayz-docker/server/savefile-backup-folder/mpmissions /home/dayz-docker/server/dayz-server/
         return 0
     elif [ -d "/home/dayz-docker/server/dayz-server/mpmissions" ] && [ -d "/home/dayz-docker/server/savefile-backup-folder/mpmissions" ]; then
-        steamcmd +force_install_dir /home/dayz-docker/server/dayz-server/ +login "$STEAM_USERNAME" +app_update 223350 +quit
+        bash /scripts/install_mods.sh
         rm -rf /home/dayz-docker/server/dayz-server/mpmissions
         cp -r /home/dayz-docker/server/savefile-backup-folder/mpmissions /home/dayz-docker/server/dayz-server/
         return 0
     else
         return 0
     fi
-    return 0
 }
 
 run_server_as_requested_user() {
     cd /home/dayz-docker/server/dayz-server/ || exit
-    ./DayZServer -config=serverDZ.cfg -port=${PORT:-2302} -BEpath=battleye -profiles=profiles -dologs -adminlog -netlog -freezecheck
+    ./DayZServer -config=serverDZ.cfg -port=${PORT:-2302} "-mod=${WORKSHOP_MODS}" -BEpath=battleye -profiles=profiles -dologs -adminlog -netlog -freezecheck
 }
 
 
 # 1. Evaluate the condition: Do we need to install?
-if [ ! -d "/home/dayz-docker/server/dayz-server/mpmissions" ]; then
+if [ ! -d "/home/dayz-docker/server/dayz-server/battleye" ]; then
     install_server_as_requested_user
 else
     echo "DayZ directory exists. Skipping installation step."
